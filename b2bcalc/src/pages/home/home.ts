e98b2e29327c22f310b1ca8e7b546a82475d1b0e
Bartosz Kosarzycki <kosiara87@gmail.com>
2017-02-03 12:09:26 +0100

    remove unused code

--- b2bcalc/src/pages/home/home.ts
+++ b2bcalc/src/pages/home/home.ts
@@ -10,16 +10,12 @@ export class HomePage {
 
   public detailsVisible = false;
 
-  public arrowRotationDeg = 90;
-
   constructor(public navCtrl: NavController) {
 
   }
 
   showOptionsClick(e) {
     this.detailsVisible = !this.detailsVisible;
-    this.arrowRotationDeg =
-      this.arrowRotationDeg == 0 ?  90 : 0;
     console.log(e);
     e.target.classList.toggle("up");
   }

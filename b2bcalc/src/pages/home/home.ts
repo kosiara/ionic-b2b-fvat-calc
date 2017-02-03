24165c07a58a1407fec6eb69d58ca584b9266344
Bartosz Kosarzycki <kosiara87@gmail.com>
2017-02-03 11:53:27 +0100

    working rotation

--- b2bcalc/src/pages/home/home.ts
+++ b2bcalc/src/pages/home/home.ts
@@ -10,11 +10,17 @@ export class HomePage {
 
   public detailsVisible = false;
 
+  public arrowRotationDeg = 90;
+
   constructor(public navCtrl: NavController) {
 
   }
 
-  showOptionsClick() {
+  showOptionsClick(e) {
     this.detailsVisible = !this.detailsVisible;
+    this.arrowRotationDeg =
+      this.arrowRotationDeg == 0 ?  90 : 0;
+    console.log(e);
+    e.target.classList.toggle("up");
   }
 }
